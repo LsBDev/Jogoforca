53f8e5ecf1b4a8c94cc089f4269b88379f5c5f24
LsB.Dev <prof.luifel@gmail.com>
2023-02-22 17:10:23 -0300

    escolha e renderização de acertos/erros

--- src/Letras.jsx
+++ src/Letras.jsx
@@ -1,19 +1,37 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./letras.css"
 
-export default function Letras ({disabled, error, setDisabled}) {
-  const array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  const [click, setClick] =  useState([])
-
+export default function Letras ({letterArray, errorCount,  setErrorCount, setLetterArray, chosenWord,  secretWord, setSecretWord, forcaImagens, setForca}) {
+  // const array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; 
+  setForca(forcaImagens[errorCount])
   function clickLetter(letter) {
-    setClick([...click, letter])
-  }
-  
+    setLetterArray((prevArray) => prevArray.map(object => {
+      if(object.id === letter.id) {
+        object.isEnabled = false
+        return object
+      } else {
+        return object
+      }
+    })
+    )
+    if(chosenWord.includes(letter.letra)) {
+      setSecretWord((prevArray) => {
+        const newArray = [...prevArray]
+        for(let i = 0; i < chosenWord.length; i++) {
+          if(chosenWord[i] === letter.letra) {
+            newArray[i] = letter.letra
+          }
+        }
+        return newArray
+      })      
+    } else {
+      setErrorCount(errorCount + 1)
+    }
+  }    
 
   return (
     <div className="letras">
-      {array.map((l, index) => <button key={index} onClick={() => clickLetter(l)} className={`letra ${disabled ? '' : 'enabled'} ${click.includes(l) ? 'disabled' : ''}`} disabled={disabled}>{l.toUpperCase()}</button>
-      )} 
+      {letterArray.map((l) => <button key={l.id} onClick={() => clickLetter(l)} className={`letra ${l.isEnabled ? 'enabled': 'disabled'}`} disabled={l.isEnabled ? false : true}>{l.letra.toUpperCase()}</button>)} 
     </div>
   )
 }
